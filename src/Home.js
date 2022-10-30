7b109c632b9464e4ae47c97b5eb72ada44da1d98
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-31 00:37:40 +0600

    Delete operation added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData()
+    const [displayUser, setDisplayUser] = useState(users)
+
+    const handleUserDelete = user => {
+        const agree = window.confirm(`Are you sure you want to delete ${user.name}`)
+        if (agree) {
+            console.log(user._id)
+
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('User deleted successfully')
+                        const remainingUser = displayUser.filter(usr => usr._id !== user._id);
+                        setDisplayUser(remainingUser);
+                    }
+                })
+
+        }
+
+    }
     return (
         <div>
-            <h3>Home page</h3>
+            <h3>User : {displayUser.length}</h3>
+            <div>
+                {
+                    displayUser.map(user => <p key={user._id}>
+                        {user.name} {user.email} <button
+                            onClick={() => { handleUserDelete(user) }}
+                        >X</button>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
